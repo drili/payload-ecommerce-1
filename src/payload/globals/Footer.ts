12861949403018ccbeabb334c58ee4ccb155613e
drili <DriliB@hotmail.com>
2024-01-01 14:53:03 +0100

    :tada: 00:43:37 - Footer

--- src/payload/globals/Footer.ts
+++ src/payload/globals/Footer.ts
@@ -3,20 +3,26 @@ import type { GlobalConfig } from 'payload/types'
 import link from '../fields/link'
 
 export const Footer: GlobalConfig = {
-  slug: 'footer',
-  access: {
-    read: () => true,
-  },
-  fields: [
-    {
-      name: 'navItems',
-      type: 'array',
-      maxRows: 6,
-      fields: [
-        link({
-          appearances: false,
-        }),
-      ],
+    slug: 'footer',
+    access: {
+        read: () => true,
     },
-  ],
+    fields: [
+        {
+            name: "copyright",
+            label: "copyright",
+            type: "text",
+            required: true,
+        },
+        {
+            name: 'navItems',
+            type: 'array',
+            maxRows: 6,
+            fields: [
+                link({
+                    appearances: false,
+                }),
+            ],
+        },
+    ],
 }
