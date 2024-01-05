bc404f5fa07427e900ac6781c677ff5b5af23ca1
drili <DriliB@hotmail.com>
2024-01-06 00:56:46 +0100

    :tada: 01:10:53 - Create Account Page

--- src/app/(pages)/create-account/page.tsx
+++ src/app/(pages)/create-account/page.tsx
@@ -8,28 +8,58 @@ import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
 import CreateAccountForm from './CreateAccountForm'
 
 import classes from './index.module.scss'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default async function CreateAccount() {
-  await getMeUser({
-    validUserRedirect: `/account?warning=${encodeURIComponent(
-      'Cannot create a new account while logged in, please log out and try again.',
-    )}`,
-  })
+    await getMeUser({
+        validUserRedirect: `/account?warning=${encodeURIComponent(
+            'Cannot create a new account while logged in, please log out and try again.',
+        )}`,
+    })
 
-  return (
-    <Gutter className={classes.createAccount}>
-      <h1>Create Account</h1>
-      <RenderParams />
-      <CreateAccountForm />
-    </Gutter>
-  )
+    return (
+        <section className={classes.createAccount}>
+            <div className={classes.heroImg}>
+                <Link href="/">
+                    <Image
+                        src="/logo-black.svg"
+                        alt=''
+                        width={250}
+                        height={23}
+                        className={classes.logo}
+                    />
+                </Link>
+            </div>
+
+            <div className={classes.formWrapper}>
+                <div className={classes.formContainer}>
+                    <RenderParams className={classes.params} />
+
+                    <div className={classes.formTitle}>
+                        <h3>Create Account</h3>
+
+                        <Image
+                            src="/assets/icons/hand.png"
+                            alt=''
+                            width={30}
+                            height={30}
+                        />
+                    </div>
+
+                    <p>Create enter details</p>
+                    <CreateAccountForm />
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export const metadata: Metadata = {
-  title: 'Account',
-  description: 'Create an account or log in to your existing account.',
-  openGraph: mergeOpenGraph({
     title: 'Account',
-    url: '/account',
-  }),
+    description: 'Create an account or log in to your existing account.',
+    openGraph: mergeOpenGraph({
+        title: 'Account',
+        url: '/account',
+    }),
 }
