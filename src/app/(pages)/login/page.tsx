537272bb8c8485f39e4cacb31ac8c253d998046b
drili <DriliB@hotmail.com>
2024-01-06 00:51:45 +0100

    :tada: 01:04:41 - Login Page

--- src/app/(pages)/login/page.tsx
+++ src/app/(pages)/login/page.tsx
@@ -8,26 +8,56 @@ import { mergeOpenGraph } from '../../_utilities/mergeOpenGraph'
 import LoginForm from './LoginForm'
 
 import classes from './index.module.scss'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default async function Login() {
-  await getMeUser({
-    validUserRedirect: `/account?warning=${encodeURIComponent('You are already logged in.')}`,
-  })
+    await getMeUser({
+        validUserRedirect: `/account?warning=${encodeURIComponent('You are already logged in.')}`,
+    })
 
-  return (
-    <Gutter className={classes.login}>
-      <RenderParams className={classes.params} />
-      <h1>Log in</h1>
-      <LoginForm />
-    </Gutter>
-  )
+    return (
+        <section className={classes.login}>
+            <div className={classes.heroImg}>
+                <Link href="/">
+                    <Image 
+                        src="/logo-black.svg"
+                        alt=''
+                        width={250}
+                        height={23}
+                        className={classes.logo}
+                    />
+                </Link>
+            </div>
+
+            <div className={classes.formWrapper}>
+                <div className={classes.formContainer}>
+                    <RenderParams className={classes.params} />
+
+                    <div className={classes.formTitle}>
+                        <h3>Welcome</h3>
+
+                        <Image 
+                            src="/assets/icons/hand.png"
+                            alt=''
+                            width={30}
+                            height={30}
+                        />
+                    </div>
+
+                    <p>Please login here</p>
+                    <LoginForm />
+                </div>
+            </div>
+        </section>
+    )
 }
 
 export const metadata: Metadata = {
-  title: 'Login',
-  description: 'Login or create an account to get started.',
-  openGraph: mergeOpenGraph({
     title: 'Login',
-    url: '/login',
-  }),
+    description: 'Login or create an account to get started.',
+    openGraph: mergeOpenGraph({
+        title: 'Login',
+        url: '/login',
+    }),
 }
