1b7dbe9694cbddb480d784ed8b945e7603e3f4d2
drili <DriliB@hotmail.com>
2024-01-09 23:36:49 +0100

    :tada: 01:19:58 - Hero Section

--- src/payload/fields/hero.ts
+++ src/payload/fields/hero.ts
@@ -6,54 +6,58 @@ import label from './richText/label'
 import largeBody from './richText/largeBody'
 
 export const hero: Field = {
-  name: 'hero',
-  label: false,
-  type: 'group',
-  fields: [
-    {
-      type: 'select',
-      name: 'type',
-      label: 'Type',
-      required: true,
-      defaultValue: 'lowImpact',
-      options: [
+    name: 'hero',
+    label: false,
+    type: 'group',
+    fields: [
         {
-          label: 'None',
-          value: 'none',
+            type: 'select',
+            name: 'type',
+            label: 'Type',
+            required: true,
+            defaultValue: 'lowImpact',
+            options: [
+                {
+                    label: 'None',
+                    value: 'none',
+                },
+                {
+                    label: 'High Impact',
+                    value: 'highImpact',
+                },
+                {
+                    label: 'Medium Impact',
+                    value: 'mediumImpact',
+                },
+                {
+                    label: 'Low Impact',
+                    value: 'lowImpact',
+                },
+                {
+                    label: 'Custom Hero',
+                    value: 'customHero',
+                },
+            ],
         },
+        richText({
+            admin: {
+                elements: ['h1', largeBody, label, 'link'],
+                leaves: [],
+            },
+        }),
+        linkGroup({
+            overrides: {
+                maxRows: 2,
+            },
+        }),
         {
-          label: 'High Impact',
-          value: 'highImpact',
+            name: 'media',
+            type: 'upload',
+            relationTo: 'media',
+            required: true,
+            admin: {
+                condition: (_, { type } = {}) => ['highImpact', 'mediumImpact', 'customHero'].includes(type),
+            },
         },
-        {
-          label: 'Medium Impact',
-          value: 'mediumImpact',
-        },
-        {
-          label: 'Low Impact',
-          value: 'lowImpact',
-        },
-      ],
-    },
-    richText({
-      admin: {
-        elements: ['h1', largeBody, label, 'link'],
-        leaves: [],
-      },
-    }),
-    linkGroup({
-      overrides: {
-        maxRows: 2,
-      },
-    }),
-    {
-      name: 'media',
-      type: 'upload',
-      relationTo: 'media',
-      required: true,
-      admin: {
-        condition: (_, { type } = {}) => ['highImpact', 'mediumImpact'].includes(type),
-      },
-    },
-  ],
+    ],
 }
